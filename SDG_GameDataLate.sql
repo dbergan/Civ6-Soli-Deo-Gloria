bcc9b9f47e936292e73be04804084687160e2c3c
DB <dbergan@gmail.com>
2019-11-08 15:41:30 -0600

    edit all religion names; fix overflow on religious screen

--- SDG_GameDataLate.sql
+++ SDG_GameDataLate.sql
@@ -1,1 +1,2 @@
 UPDATE GreatPersonIndividuals SET AreaHighlightRadius = 2 WHERE GreatPersonClassType = 'GREAT_PERSON_CLASS_PROPHET' ;
+UPDATE Religions SET RequiresCustomName = 1 WHERE ReligionType != 'RELIGION_PANTHEON' ;
