3bf9c007c8f3e785cfa41d80b5f9ded1a334418c
DB <dbergan@gmail.com>
2021-01-22 17:48:24 -0600

    2021-01-22

---
+++ GameSetup.sql
@@ -0,0 +1,8 @@
+INSERT INTO Parameters (ParameterId, Name, Description, Domain, DefaultValue,
+ConfigurationGroup, ConfigurationId, GroupId, Visible, ReadOnly,
+SupportsSinglePlayer, SupportsLANMultiplayer, SupportsInternetMultiplayer, SupportsHotSeat, SupportsPlayByCloud,
+ChangeableAfterGameStart, ChangeableAfterPlayByCloudMatchCreate, SortIndex) VALUES 
+('SDG_ALL_RELIGIONS_CUSTOM', 'SDG - All Custom Names', 'The names for all relgions can be customized, but the AI chooses blank names', 'bool', 0, 
+'Game', 'SDG_ALL_RELIGIONS_CUSTOM', 'GameOptions', 1, 0, 
+1, 1, 1, 1, 1, 
+0, 0, 81) ;
