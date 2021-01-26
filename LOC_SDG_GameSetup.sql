989b60d77ba06552bde6dba321788560e825ad8d
DB <dbergan@gmail.com>
2021-01-25 18:16:21 -0600

    2021-01-25

---
+++ LOC_SDG_GameSetup.sql
@@ -0,0 +1,57 @@
+INSERT OR REPLACE INTO LocalizedText 
+(Tag, Text, Language) VALUES
+
+('LOC_SDG_MOD_NOTE', '[NEWLINE][NEWLINE][COLOR_Red]from mod "DB [ICON_Faith]Soli Deo Gloria[ICON_Faith]"[ENDCOLOR]', 'en_US'),
+
+('LOC_SDG_OLYMPIC_PANTHEONS_NAME', 'Olympic Pantheons', 'en_US'),
+('LOC_SDG_OLYMPIC_PANTHEONS_DESC', 'Pantheon overhaul{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_REMOVE_CONDEMN_HERETIC_NAME', 'Remove Condemn Heretic', 'en_US'),
+('LOC_SDG_REMOVE_CONDEMN_HERETIC_DESC', 'Removes the Condemn Heretic action from military units{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_ONE_CLEANSE_INQUISITORS_NAME', 'Inquisitors: Only 1 Cleanse', 'en_US'),
+('LOC_SDG_ONE_CLEANSE_INQUISITORS_DESC', 'Inquisitors only get one cleansing charge each, instead of the usual three (and don''t get bonus charges from Mosques or the Exodus of the Evangelicals golden age commemoration){LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_DONT_NEED_INQUISITION_NAME', 'Inquisitors: Don''t Need An Inquisition', 'en_US'),
+('LOC_SDG_DONT_NEED_INQUISITION_DESC', 'Removes inquisitions from the game. Inquisitors can simply be purchased with faith from a Temple.{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_INQUISITORS_FROM_SHRINES_NAME', 'Inquisitors: Purchased from Shrines', 'en_US'),
+('LOC_SDG_INQUISITORS_FROM_SHRINES_DESC', 'Instead of Temples{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_RELIGIOUS_UNITS_TWO_MOVES_NAME', 'Religious Units: Only 2 Movement', 'en_US'),
+('LOC_SDG_RELIGIOUS_UNITS_TWO_MOVES_DESC', 'Missionaries, Apostles, Inquisitors and Gurus only have 2 base movement. Normally they have 4, which makes them better scouts than Scouts.{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_NO_RELIGIOUS_SEA_SPIES_NAME', 'No Religious Sea-Spies', 'en_US'),
+('LOC_SDG_NO_RELIGIOUS_SEA_SPIES_DESC', 'Religious units can only enter water tiles when they are adjacent to a naval unit. If a religious unit is embarked and naval units are no longer adjacent to it, it will die at the end of the turn.{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_ALL_HS3_BUILDINGS_NAME', 'All Holy Site Buildings', 'en_US'),
+('LOC_SDG_ALL_HS3_BUILDINGS_DESC', 'All level 3 Holy Site buildings (Cathedral, Mosque, Pagoda, etc.) are available when the Divine Right civic is obtained. The Worship ("building") class of beliefs is removed.{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_CATHEDRALS_HOLD_RELICS_NAME', 'Cathedrals Hold Relics', 'en_US'),
+('LOC_SDG_CATHEDRALS_HOLD_RELICS_DESC', 'Cathedrals can hold religious art, a relic, or a piece of music. Normally they can only hold religious art.{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_KOTOKUIN_TO_THEOLGY_NAME', 'Kotoku-In Moved To Theology', 'en_US'),
+('LOC_SDG_KOTOKUIN_TO_THEOLGY_DESC', 'Kotoku-In (wonder) moves from the Divine Right civic to the Theology civic and its production cost changes from 710 to 400{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_ALL_CUSTOM_NAMES_NAME', 'All Religions Can Have Custom Names', 'en_US'),
+('LOC_SDG_ALL_CUSTOM_NAMES_DESC', 'The names for all relgions can be customized, but the AI chooses blank names{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_DOUBLE_BELIEFS_NAME', 'Double Beliefs', 'en_US'),
+('LOC_SDG_DOUBLE_BELIEFS_DESC', 'Players get 2 beliefs for each Great Prophet instead of just 1{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_COMBAT_DOESNT_PRESSURE_NAME', 'Combat Doesn''t Pressure', 'en_US'),
+('LOC_SDG_COMBAT_DOESNT_PRESSURE_DESC', 'Theological Combat no longer affects the religious pressure in nearby cities{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_BUY_HSBUILDINGS_WITH_FAITH_NAME', 'Buy Holy Site Buildings With Faith', 'en_US'),
+('LOC_SDG_BUY_HSBUILDINGS_WITH_FAITH_DESC', 'All Holy Site buildings can be purchased with gold or faith by default{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_MARTYRS_LOSE_CS_NAME', 'Weak Martyrs', 'en_US'),
+('LOC_SDG_MARTYRS_LOSE_CS_DESC', 'Apostles with a Martyr promotion lose 100 combat strength (110 --> 10; spread strength does not change). They still create a relic upon death.{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_REMOVE_HEATHEN_CONVERSION_NAME', 'Remove Heathen Conversion', 'en_US'),
+('LOC_SDG_REMOVE_HEATHEN_CONVERSION_DESC', 'Removes the Heathen Conversion promotion from Apostles{LOC_SDG_MOD_NOTE}', 'en_US'),
+
+('LOC_SDG_RELIGION_TESTER_HELPER_NAME', 'Religion Tester', 'en_US'),
+('LOC_SDG_RELIGION_TESTER_HELPER_DESC', 'Super-Fast religion gameplay values to help me test my religion mods more quickly{LOC_SDG_MOD_NOTE}', 'en_US')
+
+;
--- SDG_GameSetup.sql
+++ SDG_GameSetup.sql
@@ -4,90 +4,90 @@ SupportsSinglePlayer, SupportsLANMultiplayer, SupportsInternetMultiplayer, Suppo
 ChangeableAfterGameStart, ChangeableAfterPlayByCloudMatchCreate, SortIndex) VALUES 
 
 
-('SDG_OLYMPIC_PANTHEONS', 'Olympic Pantheons', 'Pantheon overhaul[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_OLYMPIC_PANTHEONS', 'LOC_SDG_OLYMPIC_PANTHEONS_NAME', 'LOC_SDG_OLYMPIC_PANTHEONS_DESC', 
 'bool', 1, 'Game', 'SDG_OLYMPIC_PANTHEONS', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 320),
+0, 0, 330),
 
-('SDG_REMOVE_CONDEMN_HERETIC', 'Remove Condemn Heretic', 'Removes the Condemn Heretic action from military units[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_REMOVE_CONDEMN_HERETIC', 'LOC_SDG_REMOVE_CONDEMN_HERETIC_NAME', 'LOC_SDG_REMOVE_CONDEMN_HERETIC_DESC', 
 'bool', 1, 'Game', 'SDG_REMOVE_CONDEMN_HERETIC', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 321),
+0, 0, 331),
 
-('SDG_ONE_CLEANSE_INQUISITORS', 'Inquisitors: Only 1 Cleanse', 'Inquisitors only get one cleansing charge each, instead of the usual three (and don''t get bonus charges from Mosques or the Exodus of the Evangelicals golden age commemoration)[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_ONE_CLEANSE_INQUISITORS', 'LOC_SDG_ONE_CLEANSE_INQUISITORS_NAME', 'LOC_SDG_ONE_CLEANSE_INQUISITORS_DESC', 
 'bool', 1, 'Game', 'SDG_ONE_CLEANSE_INQUISITORS', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 322),
+0, 0, 332),
 
-('SDG_DONT_NEED_INQUISITION', 'Inquisitors: Don''t Need An Inquisition', 'Removes inquisitions from the game. Inquisitors can simply be purchased with faith from a Temple.[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_DONT_NEED_INQUISITION', 'LOC_SDG_DONT_NEED_INQUISITION_NAME', 'LOC_SDG_DONT_NEED_INQUISITION_DESC', 
 'bool', 1, 'Game', 'SDG_DONT_NEED_INQUISITION', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 323),
+0, 0, 333),
 
-('SDG_INQUISITORS_FROM_SHRINES', 'Inquisitors: Purchased from Shrines', 'Instead of Temples[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_INQUISITORS_FROM_SHRINES', 'LOC_SDG_INQUISITORS_FROM_SHRINES_NAME', 'LOC_SDG_INQUISITORS_FROM_SHRINES_DESC', 
 'bool', 1, 'Game', 'SDG_INQUISITORS_FROM_SHRINES', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 324),
+0, 0, 334),
 
-('SDG_RELIGIOUS_UNITS_TWO_MOVES', 'Religious Units: Only 2 Movement', 'Missionaries, Apostles, Inquisitors and Gurus only have 2 base movement. Normally they have 4, which makes them better scouts than Scouts.[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_RELIGIOUS_UNITS_TWO_MOVES', 'LOC_SDG_RELIGIOUS_UNITS_TWO_MOVES_NAME', 'LOC_SDG_RELIGIOUS_UNITS_TWO_MOVES_DESC', 
 'bool', 1, 'Game', 'SDG_RELIGIOUS_UNITS_TWO_MOVES', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 325),
+0, 0, 335),
 
-('SDG_NO_RELIGIOUS_SEA_SPIES', 'No Religious Sea-Spies', 'Religious units can only enter water tiles when they are adjacent to a naval unit. If a religious unit is embarked and naval units are no longer adjacent to it, it will die at the end of the turn.[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_NO_RELIGIOUS_SEA_SPIES', 'LOC_SDG_NO_RELIGIOUS_SEA_SPIES_NAME', 'LOC_SDG_NO_RELIGIOUS_SEA_SPIES_DESC', 
 'bool', 1, 'Game', 'SDG_NO_RELIGIOUS_SEA_SPIES', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 326),
+0, 0, 336),
 
-('SDG_ALL_HS3_BUILDINGS', 'All Holy Site Buildings', 'All level 3 Holy Site buildings (Cathedral, Mosque, Pagoda, etc.) are available when the Divine Right civic is obtained. The Worship ("building") class of beliefs is removed.[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_ALL_HS3_BUILDINGS', 'LOC_SDG_ALL_HS3_BUILDINGS_NAME', 'LOC_SDG_ALL_HS3_BUILDINGS_DESC', 
 'bool', 1, 'Game', 'SDG_ALL_HS3_BUILDINGS', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 327),
+0, 0, 337),
 
-('SDG_CATHEDRALS_HOLD_RELICS', 'Cathedrals Hold Relics', 'Cathedrals can hold religious art, a relic, or a piece of music. Normally they can only hold religious art.[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_CATHEDRALS_HOLD_RELICS', 'LOC_SDG_CATHEDRALS_HOLD_RELICS_NAME', 'LOC_SDG_CATHEDRALS_HOLD_RELICS_DESC', 
 'bool', 1, 'Game', 'SDG_CATHEDRALS_HOLD_RELICS', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 328),
+0, 0, 338),
 
-('SDG_KOTOKUIN_TO_THEOLGY', 'Kotoku-In Moved To Theology', 'Kotoku-In (wonder) moves from the Divine Right civic to the Theology civic and its production cost changes from 710 to 400[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_KOTOKUIN_TO_THEOLGY', 'LOC_SDG_KOTOKUIN_TO_THEOLGY_NAME', 'LOC_SDG_KOTOKUIN_TO_THEOLGY_DESC', 
 'bool', 1, 'Game', 'SDG_KOTOKUIN_TO_THEOLGY', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 329),
+0, 0, 339),
 
 
-('SDG_ALL_CUSTOM_NAMES', 'All Religions Can Have Custom Names', 'The names for all relgions can be customized, but the AI chooses blank names[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_ALL_CUSTOM_NAMES', 'LOC_SDG_ALL_CUSTOM_NAMES_NAME', 'LOC_SDG_ALL_CUSTOM_NAMES_DESC', 
 'bool', 0, 'Game', 'SDG_ALL_CUSTOM_NAMES', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 340),
+0, 0, 350),
 
-('SDG_DOUBLE_BELIEFS', 'Double Beliefs', 'Players get 2 beliefs for each Great Prophet instead of just 1[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_DOUBLE_BELIEFS', 'LOC_SDG_DOUBLE_BELIEFS_NAME', 'LOC_SDG_DOUBLE_BELIEFS_DESC', 
 'bool', 0, 'Game', 'SDG_DOUBLE_BELIEFS', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 341),
+0, 0, 351),
 
-('SDG_COMBAT_DOESNT_PRESSURE', 'Combat Doesn''t Pressure', 'Theological Combat no longer affects the religious pressure in nearby cities[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_COMBAT_DOESNT_PRESSURE', 'LOC_SDG_COMBAT_DOESNT_PRESSURE_NAME', 'LOC_SDG_COMBAT_DOESNT_PRESSURE_DESC', 
 'bool', 0, 'Game', 'SDG_COMBAT_DOESNT_PRESSURE', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 342),
+0, 0, 352),
 
-('SDG_BUY_HSBUILDINGS_WITH_FAITH', 'Buy Holy Site Buildings With Faith', 'All Holy Site buildings can be purchased with gold or faith by default[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_BUY_HSBUILDINGS_WITH_FAITH', 'LOC_SDG_BUY_HSBUILDINGS_WITH_FAITH_NAME', 'LOC_SDG_BUY_HSBUILDINGS_WITH_FAITH_DESC', 
 'bool', 0, 'Game', 'SDG_BUY_HSBUILDINGS_WITH_FAITH', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 343),
+0, 0, 353),
 
-('SDG_MARTYRS_LOSE_CS', 'Weak Martyrs', 'Apostles with a Martyr promotion lose 100 combat strength (110 --> 10; spread strength does not change). They still create a relic upon death.[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_MARTYRS_LOSE_CS', 'LOC_SDG_MARTYRS_LOSE_CS_NAME', 'LOC_SDG_MARTYRS_LOSE_CS_DESC', 
 'bool', 0, 'Game', 'SDG_MARTYRS_LOSE_CS', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 344),
+0, 0, 354),
 
-('SDG_REMOVE_HEATHEN_CONVERSION', 'Remove Heathen Conversion', 'Removes the Heathen Conversion promotion from Apostles[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_REMOVE_HEATHEN_CONVERSION', 'LOC_SDG_REMOVE_HEATHEN_CONVERSION_NAME', 'LOC_SDG_REMOVE_HEATHEN_CONVERSION_DESC', 
 'bool', 0, 'Game', 'SDG_REMOVE_HEATHEN_CONVERSION', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 345),
+0, 0, 355),
 
-('SDG_RELIGION_TESTER_HELPER', 'Religion Tester', 'Super-Fast religion gameplay values to help me test my religion mods more quickly[NEWLINE][NEWLINE][COLOR_Red]from mod "Soli Deo Gloria"[ENDCOLOR]', 
+('SDG_RELIGION_TESTER_HELPER', 'LOC_SDG_RELIGION_TESTER_HELPER_NAME', 'LOC_SDG_RELIGION_TESTER_HELPER_DESC', 
 'bool', 0, 'Game', 'SDG_RELIGION_TESTER_HELPER', 'AdvancedOptions', 1, 0, 
 1, 1, 1, 1, 1, 
-0, 0, 346)
+0, 0, 356)
 
 ;
